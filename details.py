e3825d8b8bdc9f842cb992f36a43b1c1e1534d83
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2024-01-02 08:25:10 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
 api_id = 20717621
 api_hash = "a68af155e7f0c79b1baab1f185f4181d"
-bot_token = "6812673918:AAHAMunaJJ04DUu-F5-PvAUe76qykODLO_Y"
+bot_token = "6629981026:AAGqnnK7GGTl-zBZKg3TdkgucrzPPM158nU"
 
